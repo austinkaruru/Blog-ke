a464b8e167b122afce26ee8414c514ccdc6fd220
Austin Karuru <karuruaustine@gmail.com>
2018-09-14 11:44:47 +0300

    added navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
-app.config.from_object(DevConfig)
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile("config.py")
+
+
+bootstrap = Bootstrap(app)
+
+
+# app.config.from_object(DevConfig)
 
 from app import views
