c40dd9d0c83b6244f0a96bd1bec75671492ea949
Austin Karuru <karuruaustine@gmail.com>
2018-09-14 14:32:51 +0300

    error py is working,and have commented out a few codes

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 # from .models import blogs
-# from .forms import BlogForm
+from .forms import BlogForm
 # Blog = blogs.Blog
 
 
@@ -11,7 +11,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/blog/<int:blog_id>')
-def blog(blog_id):
+# @app.route('/blog/<int:blog_id>')
+# def blog(blog_id):
 
-    return render_template('new_blog.html', id=blog_id)
+#     return render_template('blog.html', id=blog_id)
+# @app.route('/blog/<int:id>') methods = ['GET', 'POST'])
+# def new_blog(id):
+#     form=BlogForm()
+
+#     if form.validate_on_submit():
+#         title=form.title.data
+#         blog=form.blog.data
+#         new_blog=Blog(title)
+#         new_blog.save_blog()
+
+#     return render_template('new_blog,html', title = title, blog_form = form)
