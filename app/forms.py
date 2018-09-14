5976dea5711117a256e064d1792c456ebc5d8d9e
Austin Karuru <karuruaustine@gmail.com>
2018-09-14 12:30:23 +0300

    trying to display blogs

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Review title', validators=[Required()])
+    blog = TextAreaField('New Blog', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from flask import render_template
 from app import app
+# from .models import blogs
+# from .forms import BlogForm
+# Blog = blogs.Blog
 
 
 @app.route('/')
 def index():
 
     return render_template('index.html')
+
+
+@app.route('/blog/<int:blog_id>')
+def blog(blog_id):
+
+    return render_template('new_blog.html', id=blog_id)
