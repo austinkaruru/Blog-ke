00e824b3762d773890df0cd0b69501984c671207
Austin Karuru <karuruaustine@gmail.com>
2018-09-17 07:30:05 +0300

    new

--- app/views.py
+++ app/views.py
@@ -1,28 +1,28 @@
-from flask import render_template
-from app import app
-# from .models import blogs
-from .forms import BlogForm
-# Blog = blogs.Blog
+# from flask import render_template
+# from app import app
+# # from .models import blogs
+# from .forms import BlogForm
+# # Blog = blogs.Blog
 
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
-    return render_template('index.html')
+#     return render_template('index.html')
 
 
-# @app.route('/blog/<int:blog_id>')
-# def blog(blog_id):
+# # @app.route('/blog/<int:blog_id>')
+# # def blog(blog_id):
 
-#     return render_template('blog.html', id=blog_id)
-# @app.route('/blog/<int:id>') methods = ['GET', 'POST'])
-# def new_blog(id):
-#     form=BlogForm()
+# #     return render_template('blog.html', id=blog_id)
+# # @app.route('/blog/<int:id>') methods = ['GET', 'POST'])
+# # def new_blog(id):
+# #     form=BlogForm()
 
-#     if form.validate_on_submit():
-#         title=form.title.data
-#         blog=form.blog.data
-#         new_blog=Blog(title)
-#         new_blog.save_blog()
+# #     if form.validate_on_submit():
+# #         title=form.title.data
+# #         blog=form.blog.data
+# #         new_blog=Blog(title)
+# #         new_blog.save_blog()
 
-#     return render_template('new_blog,html', title = title, blog_form = form)
+# #     return render_template('new_blog,html', title = title, blog_form = form)
