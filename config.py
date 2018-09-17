cdb6497f91157f0b32a1e23da513597a5b75b9a5
Austin Karuru <karuruaustine@gmail.com>
2018-09-17 10:06:47 +0300

    w

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = 2700
 
 
 class ProdConfig(Config):
